f9f13fff6c0b0445d0ac40d688dcbefa2865de19
Javier MARTINEZ LOPEZ <javier.martinez@um.es>
2013-11-26 12:19:42 +0100

    Basic model running

--- R/spdynmod.R
+++ R/spdynmod.R
@@ -101,50 +101,5 @@ spdynmod<-function(t,init,parameters,nr,nc) {
 
 	 list(c(dsm,des,drb,dbaresoil))
 }
-##############################################
-##############################################
-# SPATIAL PARAMETERS #
-require(raster)
-
-rpath = system.file("extdata",package="spdynmod")
-#rpath = '/net/netapp2/H05_Homes/majavie/spdynmod/inst/extdata'
-
-r<- raster(paste(rpath,'/mc84_1_reclass.asc',sep=''))
-
-fak<<- raster(paste(rpath,'/log_cr10_acum_rm_t1_aver.asc',sep=''))
-
-dr1<<- raster(paste(rpath,'/rambla11_cr10_dist_t1.asc',sep='')) 
-dr2<<- raster(paste(rpath,'/rambla22_cr10_dist_t1.asc',sep='')) 
-
-avd<<- raster(paste(rpath,'/ramblas_cr10_dist_t1_ave.asc',sep=''))
-
-fa_avd<<-fak+(1-avd)
-
-es_init<<-raster(paste(rpath,'/mc84_1_reclass3.asc',sep=''))
-
-sm_init<<-raster(paste(rpath,'/mc84_2_reclass3.asc',sep=''))
-
-rb_init<<-raster(paste(rpath,'/mc84_34.asc',sep=''))
-
-baresoil_init<<-raster(paste(rpath,'/mc84_4_reclass3.asc',sep=''))
-
-nr<<-dim(r)[1]
-nc<<-dim(r)[2]
-NN<<-nr*nc
-
-### state variables ###
-st <<- c(raster::as.vector(raster::as.matrix(sm_init)), raster::as.vector(raster::as.matrix(es_init)),raster::as.vector(raster::as.matrix(rb_init)),raster::as.vector(raster::as.matrix(baresoil_init)))
-
-### parameters ###
-parms <<- c(tprb = 0.005, tpsm = 0.2)# tpsm = 0.01
-
-### model specs and execution ###
-##source('mc_dynmodv2_7_functions.R')
-DT <<- 0.25
-time <- seq(0.001,24,DT)
-out <<- deSolve::ode.2D(func=spdynmod,y=st,times=time,parms=parms,method='euler',nspec = 4, dimens = c(nr, nc),nr=nr,nc=nc,names=c('Salt marsh','Salt steppe','Reed beds','Bare soil'))
-#
-
-
 
 #system('aplay -t wav hangout_dingtone.wav')
