5be8d68daa75f3cbfc2753e4b275c44f90660028
Javier MARTINEZ LOPEZ <javier.martinez@um.es>
2014-10-07 17:25:26 +0200

    last

---
+++ inst/maps_validation/mrgf.R
@@ -0,0 +1,58 @@
+mrgf<-function(a='',b='',w1,w2,year='',k=0){
+require(raster)
+af<-raster(a)#paste(a,'.asc',sep=''))
+bf<-raster(b)#paste(b,'.asc',sep=''))
+af@nrows->nr
+af@ncols->nc
+print(nr)
+print(nc)
+ft<<-NULL
+fwes<-NULL
+ees<-NULL
+fw<<-NULL
+#fitw<-NULL
+g<-0
+for(w in seq(w1,w2,2)){
+g<-g+1
+getValuesFocal(af,ngb=w)->valss1
+getValuesFocal(bf,ngb=w)->valss2
+print(paste(w,dim(valss1)[2]))
+h<-0
+fits<-NULL
+for(j in 1:dim(valss1)[1]){
+ads<-NULL
+valsx1<-valss1[j,]
+valsx2<-valss2[j,]
+vals1<-valsx1[!is.na(valsx1)]
+vals2<-valsx2[!is.na(valsx2)]
+lvals1<-length(vals1)
+cats<-unique(vals1)
+lcats<-length(cats)
+if(lcats>0){
+ h<-h+1
+ for(i in 1:lcats){
+  length(vals1[vals1==cats[i]])->a1
+  length(vals2[vals2==cats[i]])->a2
+  ad<-abs(a1-a2)
+  ads[i]<-ad
+ }
+ fit<-1-(sum(ads,na.rm=T)/(2*lvals1))
+ fits[h]<-fit
+}
+}
+fitw<-sum(fits,na.rm=T)/length(fits)
+fwe<-fitw*exp(-k*(w-1))
+ee<-exp(-k*(w-1))
+fw[g]<<-fitw
+fwes[g]<-fwe
+ees[g]<-ee
+}
+sfwes<-sum(fwes)
+sess<-sum(ees)
+ft<<-sfwes/sess
+png(paste('mrgf_',year,'.png',sep=''))
+plot(seq(w1,w2,2),fw,xlab='window size',ylab='Fw',ty='o',main='Multiple Resolution Goodness of Fit',sub=paste('Ft =',round(ft,2),'; k = ',k))
+mtext(year,side=3)
+dev.off()
+return(ft)
+}
