80df845c28c2481c49bba9ce9a6eb01561250185
Javier Martinez <javier.martinez@um.es>
2015-12-29 13:21:29 +0100

    new version 1.1.3

--- build_test.sh
+++ build_test.sh
@@ -1,4 +1,4 @@
 # symbolic link of extdata when documenting
 # sudo add-apt-repository ppa:anton+/photo-video-apps
 R CMD build --compact-vignettes='gs+qpdf' spdynmod
-R CMD check --as-cran spdynmod_1.1.2.tar.gz
+R CMD check --as-cran spdynmod_1.1.3.tar.gz
